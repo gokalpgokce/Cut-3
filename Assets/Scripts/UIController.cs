b3e8388528feefd95d2589eccde5cee7d4f88972
Gokhan <gkngkc@gmail.com>
2022-09-16 12:26:17 +0300

    Get row col count from user

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
     [Header("Scene References")]
     public GameObject mainUIGO;
+    
+    public TMP_InputField rowInputField;
+    public TMP_InputField colInputField;
     
     public void ShowMainUI()
     {
@@ -18,6 +22,26 @@ public class UIController : MonoBehaviour
     public void OnPlayClicked()
     {
         HideMainUI();
+        
+        int rowCount = 9;
+        int colCount = 9;
+        try
+        {
+            rowCount = int.Parse(rowInputField.text);
+            colCount = int.Parse(colInputField.text);
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Invalid row col count.");
+        }
+        
+        if (rowCount < 3 || colCount < 3 || rowCount > 9 || colCount > 9)
+        {
+            Debug.Log("Row Col count should be between 3-9");
+        }
+        
+        rowCount = Mathf.Clamp(rowCount, 3, 9);
+        colCount = Mathf.Clamp(colCount, 3, 9);
         
         Game.Instance.PlayGame();
     }

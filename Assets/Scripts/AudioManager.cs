fbd15fbf90524a97126e9973cb60935944c1da41
gokalpgokce <gokalpgokce7@gmail.com>
2022-10-19 15:03:33 +0300

    Add Menu Sound Track

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource audioSource;
+    public AudioSource sfxAudioSource;
+    public AudioSource menuAudioSource;
     private AudioClip _swipeClip;
     private AudioClip _clickClip;
     private AudioClip _dropClip;
@@ -19,35 +20,45 @@ public class AudioManager : MonoBehaviour
         _dropClip = Resources.Load<AudioClip>("Drop");
         _explosionClip = Resources.Load<AudioClip>("SpecialExp");
         _fireworksClip = Resources.Load<AudioClip>("Fireworks");
-        audioSource = GetComponent<AudioSource>();
+        sfxAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMenuSound()
+    {
+        menuAudioSource.Play();
+    }
+    
+    public void StopMenuSound()
+    {
+        menuAudioSource.Stop();
     }
 
     public void PlaySwipeSound()
     {
-        audioSource.PlayOneShot(_swipeClip);
+        sfxAudioSource.PlayOneShot(_swipeClip);
     }
 
     public void PlayClickSound()
     {
-        audioSource.PlayOneShot(_clickClip);
+        sfxAudioSource.PlayOneShot(_clickClip);
     }
     public void PlayDropSound()
     {
-        audioSource.PlayOneShot(_dropClip);
+        sfxAudioSource.PlayOneShot(_dropClip);
     }
     public void PlayExplosionSound()
     {
-        audioSource.PlayOneShot(_explosionClip);
+        sfxAudioSource.PlayOneShot(_explosionClip);
     }
     public void PlayFireworksSound()
     {
-        audioSource.clip = _fireworksClip;
-        audioSource.Play();
+        sfxAudioSource.clip = _fireworksClip;
+        sfxAudioSource.Play();
     }
 
     public void StopFireworksSound()
     {
-        audioSource.Stop();
-        audioSource.clip = null;
+        sfxAudioSource.Stop();
+        sfxAudioSource.clip = null;
     }
 }
